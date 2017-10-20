8b854dfa13b6f61465b1201d1cc12b28fbe93832
beryindo <bery.indo@gmail.com>
2017-10-20 12:39:54 +0700

    Update Split_Karakter.ino

--- Split_Karakter.ino
+++ Split_Karakter.ino
@@ -1,8 +1,8 @@
 void setup() {
   Serial.begin(9600);
-  char KarakterSplit[] = "cek,*635*1#";
-  char* kata0 = strtok(KarakterSplit,",");
-  char* kata1 = strtok(NULL,",");
+  char KarakterSplit[] = "kata0,kata1";
+  char* kata0 = strtok(KarakterSplit,","); //pembatas koma
+  char* kata1 = strtok(NULL,","); //pembatas koma
   Serial.println(kata0);
   Serial.println(kata1);
 }
